ac55765201d4d7b30b5de694b253ceed44f05783
Cofica17 <63316032+Cofica17@users.noreply.github.com>
2023-07-29 13:07:05 +0200

    added temp, description and min,max temp data to the main screen

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -32,7 +32,14 @@ class MainActivity : AppCompatActivity() {
     fun setData(weather_data:WeatherData){
         val locationView = findViewById<TextView>(R.id.location_name)
         val tempView = findViewById<TextView>(R.id.temp)
+        val descView = findViewById<TextView>(R.id.description)
+        val highTempView = findViewById<TextView>(R.id.high_temp)
+        val lowTempView = findViewById<TextView>(R.id.low_temp)
+
         locationView.text = weather_data.name
-        tempView.text = Utilities.convertKelvinToCelsius(weather_data.main.temp).toInt().toString()
+        tempView.text = Utilities.convertKelvinToCelsius(weather_data.main.temp).toInt().toString()+"°"
+        descView.text = weather_data.weather.get(0).description
+        highTempView.text = "H:"+Utilities.convertKelvinToCelsius(weather_data.main.temp_max).toInt().toString()+"°"
+        lowTempView.text = "L:"+Utilities.convertKelvinToCelsius(weather_data.main.temp_min).toInt().toString()+"°"
     }
 }
